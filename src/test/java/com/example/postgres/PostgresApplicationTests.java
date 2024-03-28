10ae99b4a6019cf4a8a4317afcccb2a886dc316c
Invasiion <invassiion@yandex.ru>
2024-03-28 19:13:53 +0700

    added 2 tests

--- src/test/java/com/example/postgres/PostgresApplicationTests.java
+++ src/test/java/com/example/postgres/PostgresApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.postgres;
 
+import com.example.postgres.user.dto.UserResponse;
+import com.example.postgres.user.entity.UserEntity;
+import com.example.postgres.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PostgresApplicationTests {
+@Autowired
+private UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
 	}
+
+	void repositoryTest() {
+		UserEntity user = UserEntity.builder()
+				.lastName("Denis")
+				.firstName("Kovalinskii")
+				.build();
+
+		user = userRepository.save(user);
+
+		UserEntity check = userRepository.findById(user.getId()).get();
+
+		assert check.getId().equals(user.getId());
+	}
+
 
 }
---
+++ src/test/java/com/example/postgres/WebTests.java
@@ -0,0 +1,44 @@
+package com.example.postgres;
+
+import com.example.postgres.user.entity.UserEntity;
+import com.example.postgres.user.repository.UserRepository;
+import com.example.postgres.user.routes.UserRoutes;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.print.attribute.standard.Media;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WebTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void contextLoad() throws Exception {
+        UserEntity user = UserEntity.builder()
+                .firstName("Denis")
+                .lastName("Kovalinskii")
+                .build();
+        user = userRepository.save(user);
+
+        mockMvc.perform(get(UserRoutes.BY_ID, user.getId().toString()).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
